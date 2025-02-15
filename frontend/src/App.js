f441f9354085446b792bfa05e8b112cb060da3e7
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2025-02-15 16:39:58 +0300

    Add check for photo's URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import SeminarCard from "./components/SeminarCard/SeminarCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import seminarDefaultPhoto from "./images/seminar.jpg";
 
 function App() {
-  const [seminars, setSeminars] = useState({});
+  const [seminars, setSeminars] = useState([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -13,6 +14,18 @@ function App() {
       .then((resp) => setSeminars(resp.data))
       .catch((err) => setError(err));
   }, []);
+
+  // Проверка URL для seminar.photo: url рабочий - возврат true, иначе false
+  function checkPhotoURL(photoURL) {
+    axios
+      .get(photoURL)
+      .then((resp) => {
+        return true;
+      })
+      .catch((err) => {
+        return false;
+      });
+  }
 
   return (
     <div className="App">
@@ -23,7 +36,11 @@ function App() {
           return (
             <SeminarCard
               key={seminar.id}
-              sCardImg={seminar.photo}
+              sCardImg={
+                checkPhotoURL(seminar.photo)
+                  ? seminar.photo
+                  : seminarDefaultPhoto
+              }
               sCardHeader={seminar.title}
               sCardDescription={seminar.description}
               sCardDate={seminar.date}
