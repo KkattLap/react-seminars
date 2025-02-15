5d24b6e2d5b0a1fabdbd71cee4b4d691d3398909
Ekaterina Lapenko <ekaterinalapenko8960@gmail.com>
2025-02-15 13:10:31 +0300

    Add axios get request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
+import SeminarCard from "./components/SeminarCard/SeminarCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [seminars, setSeminars] = useState({});
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/seminars")
+      .then((resp) => setSeminars(resp.data))
+      .catch((err) => setError(err));
+  }, []);
+
+  return (
+    <div className="App">
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        seminars.map((seminar) => {
+          return (
+            <SeminarCard
+              key={seminar.id}
+              sCardImg={seminar.photo}
+              sCardHeader={seminar.title}
+              sCardDescription={seminar.description}
+              sCardDate={seminar.date}
+              sCardTime={seminar.time}
+            ></SeminarCard>
+          );
+        })
+      )}
+    </div>
+  );
 }
 
 export default App;
